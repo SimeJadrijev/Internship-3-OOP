6636b75512a858713ef9b078d83042bb07e6d977
SimeJadrijev <sime.jadrijev@gmail.com>
2023-11-25 20:23:47 +0100

    Implemented initial versions of 'Call.cs' and 'Contact.cs' classes

---
+++ Internship-3-OOP/Internship-3-OOP/Call.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internship_3_OOP
+{
+    public class Call
+    {
+        public Call()
+        {
+                
+        }
+        public DateTime CallStart { get; set; }
+        public enum CallStatus
+        {
+            u_tijeku,
+            propusten,
+            zavrsen
+        }
+    }
+}
